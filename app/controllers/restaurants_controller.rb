a15ce36e7ba87252164b821c003ce0d1f588d58e
mihaisacalianu <mihaisacalianu@gmail.com>
2025-02-26 17:01:19 +0000

    Advanced Routing - Yelp MVP - made design touches an updated error message fields cannot be blank

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -12,8 +12,11 @@ class RestaurantsController < ApplicationController
 
   def create
     @restaurant = Restaurant.new(restaurant_params)
-    @restaurant.save!
+   if @restaurant.save
     redirect_to restaurant_path(@restaurant)
+   else
+    render :new, status: :unprocessable_entity
+   end
   end
 
   private
